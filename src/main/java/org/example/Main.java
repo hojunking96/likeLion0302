e0a5aa04b1c6042e22f9e1cc7136d4eea4a62194
hojunking96 <hojunking96@gmail.com>
2023-03-02 09:38:59 +0900

    5단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,38 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    static class Phrase {
+        private int number;
+        private String writer;
+        private String sentence;
+
+        public Phrase(int number, String writer, String sentence) {
+            this.number = number;
+            this.writer = writer;
+            this.sentence = sentence;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getWriter() {
+            return writer;
+        }
+
+        public String getSentence() {
+            return sentence;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Phrase> phrases = new ArrayList<>();
+
         System.out.println("== 명언 앱 ==");
         int phraseCnt = 0;
         while (true) {
@@ -15,13 +43,26 @@ public class Main {
             if (input.equals("종료")) {
                 break;
             } else if (input.equals("등록")) {
+                phraseCnt++;
                 System.out.print("명언 : ");
-                String phrase = br.readLine();
+                String sentence = br.readLine();
 
                 System.out.print("작가 : ");
                 String writer = br.readLine();
-                System.out.println(++phraseCnt + "번 명언이 등록되었습니다.");
+
+                phrases.add(new Phrase(phraseCnt, writer, sentence));
+                System.out.println(phraseCnt + "번 명언이 등록되었습니다.");
+            } else if (input.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = phrases.size() - 1; i >= 0; i--) {
+                    Phrase phrase = phrases.get(i);
+                    System.out.println(phrase.getNumber() + " / " + phrase.getWriter() + " / " + phrase.getSentence());
+                }
             }
+
         }
+
+
     }
 }
