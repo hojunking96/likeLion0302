62007f1697aff44d0c69fb1cb4c70c1eca1da2c8
hojunking96 <hojunking96@gmail.com>
2023-03-02 10:15:05 +0900

    7단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     static class Phrase {
-        private int number;
         private String writer;
         private String sentence;
 
-        public Phrase(int number, String writer, String sentence) {
-            this.number = number;
+        public Phrase(String writer, String sentence) {
             this.writer = writer;
             this.sentence = sentence;
-        }
-
-        public int getNumber() {
-            return number;
         }
 
         public String getWriter() {
@@ -33,7 +29,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Phrase> phrases = new ArrayList<>();
+        Map<Integer, Phrase> phrases = new HashMap<>();
 
         System.out.println("== 명언 앱 ==");
         int phraseCnt = 0;
@@ -50,19 +46,23 @@ public class Main {
                 System.out.print("작가 : ");
                 String writer = br.readLine();
 
-                phrases.add(new Phrase(phraseCnt, writer, sentence));
+                phrases.put(phraseCnt, new Phrase(writer, sentence));
                 System.out.println(phraseCnt + "번 명언이 등록되었습니다.");
             } else if (func.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for (int i = phrases.size() - 1; i >= 0; i--) {
+                for (int i = phrases.size(); i >= 1; i--) {
                     Phrase phrase = phrases.get(i);
-                    System.out.println(phrase.getNumber() + " / " + phrase.getWriter() + " / " + phrase.getSentence());
+                    System.out.println(i + " / " + phrase.getWriter() + " / " + phrase.getSentence());
                 }
             } else if (func.substring(0, 2).equals("삭제")) {
                 int deleteID = Integer.parseInt(func.substring(6));
-                phrases.remove(deleteID - 1);
-                System.out.println(deleteID + "번 명언이 삭제되었습니다.");
+                if (phrases.containsKey(deleteID)) {
+                    phrases.remove(deleteID);
+                    System.out.println(deleteID + "번 명언이 삭제되었습니다.");
+                } else {
+                    System.out.println(deleteID + "번 명언은 존재하지 않습니다.");
+                }
             }
         }
 
