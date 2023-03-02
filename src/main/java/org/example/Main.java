eeb2b4e2cef61633c026d93cc5bee50b78d30c7f
hojunking96 <hojunking96@gmail.com>
2023-03-02 09:17:37 +0900

    1단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("종료")) {
+                break;
+            }
+        }
     }
 }
